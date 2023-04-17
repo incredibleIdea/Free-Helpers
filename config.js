72df9e3661b00fc207a0c181d6866d00b9a840a3
incredibleIdea <510996577@qq.com>
2023-04-17 14:06:20 +0800

    change uri

---
+++ config.js
@@ -0,0 +1,23 @@
+const e = {
+  zh: "./_locales/zh_CN/messages.json",
+  en: "./_locales/en_US/messages.json"
+}, s = [
+  {
+    name: "Lazy Baker",
+    version: "1.2.0",
+    uri: "./public/blenderAddons/BystedtsBlenderBaker_1_2_0_(2.93).zip",
+    homepage: "https://blendermarket.com/products/lazy-baker"
+  },
+  {
+    name: "Auto Eye",
+    version: "0.3.2",
+    uri: "./public/blenderAddons/auto_eye_v03_2_addon.zip",
+    homepage: "https://blendermarket.com/products/auto-eye"
+  }
+], o = {
+  locales: e,
+  blender: s
+};
+export {
+  o as default
+};
